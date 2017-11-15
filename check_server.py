6ae5483197e73092f554cd3c2fe0f39842cb8dc0
Patrick Eff <Patrick.Eff@motorolasolutions.com>
2017-11-16 08:53:11 +1030

    update

--- check_server.py
+++ check_server.py
@@ -1,56 +1,72 @@
 #!/usr/bin/python
 
+###### Motorola Solutions #######
+#				#	
+#	Author: Patrick Eff	#
+#	Date: 11/15/2017	#
+#				#
+#################################
+
 import sys
 import socket
 import xml.etree.ElementTree as ET
-
-XML_FILE = "open.xml" 
-errorFD = open("errors.txt","w")
-tree = ET.parse(XML_FILE)
-root = tree.getroot()
 
 class Node:
 
 	def __init__(self,hostName,port):
 		self.hostName = hostName
 		self.port = port
-
 	def getHostName(self):
 		return self.hostName
-
 	def getPort(self):
 		return self.port
 
-def logError(hostName,port):
-
-	errorFD.write("Failed to connect to: %s:%s\n" %(hostName,port))
+def generateEmail(aList):
+	fd = open("error.txt","w")
+	for element in aList:
+		fd.write("Connection Failed on %s:%s\n" %(element.getHostName(),element.getPort()))
+	fd.close()	
+	
 
 ############ MAIN ##############
+
+exitCode = 0
+XML_FILE = "servers.xml" 
+tree = ET.parse(XML_FILE)
+root = tree.getroot()
+
 
 aList = []
 for node in root:
 
 	hostName = node.get("hostname")
-	ipAddr = socket.gethostbyname(hostName)
+	
+	try:
+		ipAddr = socket.gethostbyname(hostName)
+	except:
+		print "Failed to resolve hostname %s" %(hostName)
+		continue
 
 	for i in node.iter("port"):
 
 		port = int(i.text)
+		aNode = Node(hostName,port)
+
 		sockFD = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 		sockFD.settimeout(3)
 
-		aNode = Node(hostName,port)
 		try:
-
 			sockFD.connect((ipAddr,port))
 			print "Connected to %s:%s" %(hostName,port)
 		except:
 			aList.append(aNode)
+			exitCode = 1
 
 		sockFD.close()
 
-errorFD.close()
-#if len(aList) > 0:
+if len(aList) > 0:
+	generateEmail(aList)
+else:
+	print "Successfully connected to all servers"
 
-for element in aList:
-	print "Connection Failed on %s:%s" %(element.getHostName(),element.getPort())
+sys.exit(exitCode)
