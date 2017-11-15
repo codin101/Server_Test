b18eaac696004765848737af0915adddba253a2d
test <test@test-VirtualBox.(none)>
2017-11-16 01:52:00 +1030

    updates

--- check_server.py
+++ check_server.py
@@ -1,16 +1,43 @@
 #!/usr/bin/python
 
+import sys
 import socket
 import xml.etree.ElementTree as ET
 
 xmlFile = "open.xml" 
+errorFD = open("errors.txt","w")
 
 tree = ET.parse(xmlFile)
 root = tree.getroot()
 
+def logError(a,b):
+
+	outFD.write("Could not connect to: ",a,b)
+
+######### MAIN #########
+
 for node in root:
+
 	hostName = node.get("hostname")
-	print hostName
+	ipAddr = socket.gethostbyname(hostName)
+
 	for i in node.iter("port"):
+
 		port = int(i.text)
-		print port
+		sockFD = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+		sockFD.settimeout(3)
+
+		try:
+
+			sockFD.connect((ipAddr,port))
+			print "Connected to: ", hostName,port
+			#sockFD.close()
+
+		except:
+			logError(hostName,port)
+
+		sockFD.close()
+
+
+
+outFD.close()
