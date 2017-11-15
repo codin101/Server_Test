3803db5569c4a3090d6a4f6e575420ecbfc13b13
Patrick Eff <Patrick.Eff@motorolasolutions.com>
2017-11-16 07:03:04 +1030

    update

--- check_server.py
+++ check_server.py
@@ -4,20 +4,30 @@ import sys
 import socket
 import xml.etree.ElementTree as ET
 
-numErrors = 0
-xmlFile = "open.xml" 
+XML_FILE = "open.xml" 
 errorFD = open("errors.txt","w")
-tree = ET.parse(xmlFile)
+tree = ET.parse(XML_FILE)
 root = tree.getroot()
 
-def logError(a,b):
+class Node:
 
-	print("Error on",a,b)
+	def __init__(self,hostName,port):
+		self.hostName = hostName
+		self.port = port
 
+	def getHostName(self):
+		return self.hostName
 
+	def getPort(self):
+		return self.port
+
+def logError(hostName,port):
+
+	errorFD.write("Failed to connect to: %s:%s\n" %(hostName,port))
 
 ############ MAIN ##############
 
+aList = []
 for node in root:
 
 	hostName = node.get("hostname")
@@ -29,15 +39,18 @@ for node in root:
 		sockFD = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 		sockFD.settimeout(3)
 
+		aNode = Node(hostName,port)
 		try:
 
 			sockFD.connect((ipAddr,port))
 			print "Connected to %s:%s" %(hostName,port)
 		except:
-			logError(hostName,port)
-			numErrors = numErrors + 1
+			aList.append(aNode)
 
 		sockFD.close()
 
 errorFD.close()
-print "%s errors found" %(numErrors)
+#if len(aList) > 0:
+
+for element in aList:
+	print "Connection Failed on %s:%s" %(element.getHostName(),element.getPort())
