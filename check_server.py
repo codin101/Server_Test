c815ade5bca0c0159aacd578ffe1e9f7b90ab76a
test <test@test-VirtualBox.(none)>
2017-11-16 00:40:11 +1030

    update

--- check_server.py
+++ check_server.py
@@ -8,13 +8,9 @@ xmlFile = "open.xml"
 tree = ET.parse(xmlFile)
 root = tree.getroot()
 
-for child in root:
-    # we have a child
-    # now,...... access the child's attribute ( it should be a <node> )
-    hostName = child.attrib ## This is 'hostname="wbal.com" -> will print "wbal.com"
-    # for each child, print their elements
-    for i in child.iter("port"):
-        port = int(i.text)
-        sockFD = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        ipBinary = socgethostbyname(hostName)
-        sockFD.connect(ipBinary,port)
+for node in root:
+	hostName = node.get("hostname")
+	print hostName
+	for i in node.iter("port"):
+		port = int(i.text)
+		print port
