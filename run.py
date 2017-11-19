d75d7a79d13463a05e972e2ab9b39d99f143e14b
Patrick Eff <effp1225@gmail.com>
2017-11-18 19:11:24 -0500

    update

--- run.py
+++ run.py
@@ -4,20 +4,76 @@ import sys
 import socket
 import xml.etree.ElementTree as ET
 
+global exitCode
+exitCode = 0
+
+# Node class has:
+# hostName
+# array of Ports
+
 class Node:
 
-	def __init__(self,hostName,portList=None):
-		self.hostName = hostName
-		self.ports = portList or []
+    def __init__(self,hostName, portList = None):
+        self.hostName = hostName
+        self.portList = portList
+    
+    def getHostName(self):
+        return self.hostName
 
-	def getHostName(self):
-		return self.hostName
-
-	def getPorts(self):
-		return self.ports
+    def getPorts(self):
+        return self.portList
 
 
-exitCode = 0
+def emailFailList(failList):
+
+    global exitCode
+    exitCode = 1
+
+    for i in failList:
+
+        hostName = i.getHostName()
+        portList = i.getPorts()
+        if not portList:
+            print "Could not Resolve Host: " + hostName
+        else:
+            for j in portList:
+                print "Could not connect to " + hostName + ":" + str(j)
+        
+
+def checkServers(nodeList):
+
+    failList = []
+    for e in nodeList:
+
+        hostName = e.getHostName()
+
+        try:
+            ipAddr = socket.gethostbyname(hostName)
+        except:
+            failList.append(Node(hostName,None))
+            continue 
+
+        portList = []
+        for port in e.getPorts():
+
+                sockFD = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+		sockFD.settimeout(3)
+
+                try:
+                    sockFD.connect((ipAddr,port))
+                except:
+                    portList.append(port)
+
+
+                sockFD.close()
+        
+        failList.append(Node(hostName,portList))
+
+    if failList:
+        emailFailList(failList)
+
+#### MAIN ####
+
 XML_FILE = "servers.xml"
 tree = ET.parse(XML_FILE)
 root = tree.getroot()
@@ -26,8 +82,8 @@ nodeList= []
 for node in root:
 
 	hostName = node.get("hostname")
-	
-	portList = []
+
+        portList = []
 	for i in node.iter("port"):
 
 		port = int(i.text)
@@ -35,12 +91,8 @@ for node in root:
 	
 	aNode = Node(hostName,portList)
 	nodeList.append(aNode)
-	
-
-for i in nodeList:
-	print 
 
 
-
+checkServers(nodeList)
 
 sys.exit(exitCode)
