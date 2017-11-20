135206b6038d7faa790c47317b9b3878b82edbb7
Patrick Eff <effp1225@gmail.com>
2017-11-20 11:33:14 -0500

    test

--- test.py
+++ test.py
@@ -2,17 +2,14 @@
 
 import os
 import subprocess
+import smtplib
 
-#h = "google.com"
-#r = os.system("ping -c 2 %s" %h)
+mail = smtplib.SMTP("localhost")
+msg = "<html><head><title>Fail</title></head></html>"
+r = "Patrick.Eff@motorolasolutions.com"
+s = "Patrick.Eff@motorolasolutions.com"
 
-class J:
-
-	def __init__(self,h):
-		self.h = h	
-	def get(self):
-		return self.h
-
+mail.sendmail(s,r,msg)
 
 	
 #r = os.system("ping -c 2 " + "google.com" + " 2>/dev/null >/dev/null")
